b5b432849c79676370fb3fb81d7580e23b21a9af
Jimmy Miao <jimmymiao08@gmail.com>
2023-11-06 23:46:41 -0600

    Updates

---
+++ precache-manifest.f983ed1c51e791fc9d3ab268d8085f2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f0970922dafc7e1b5237a63671960a",
+    "url": "/jierm2/JYM/index.html"
+  },
+  {
+    "revision": "3f2d5972dc7b61a5b14a",
+    "url": "/jierm2/JYM/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "22ffa99203a149615667",
+    "url": "/jierm2/JYM/static/css/main.b0fa59df.chunk.css"
+  },
+  {
+    "revision": "3f2d5972dc7b61a5b14a",
+    "url": "/jierm2/JYM/static/js/2.3a5c99e3.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/jierm2/JYM/static/js/2.3a5c99e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22ffa99203a149615667",
+    "url": "/jierm2/JYM/static/js/main.dc4a4f70.chunk.js"
+  },
+  {
+    "revision": "cfdac2b99ebdbc06f77e",
+    "url": "/jierm2/JYM/static/js/runtime-main.0fd89424.js"
+  }
+]);
